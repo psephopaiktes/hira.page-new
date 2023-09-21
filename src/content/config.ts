4618bf3723e075af64d2947d15a9f7f69f1a115f
Akira HIRATA <hirataakira6@gmail.com>
2023-09-21 17:56:44 +0900

    Component Filter

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,17 @@
 import { z, defineCollection } from "astro:content";
 
 const blog = defineCollection({
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-    tags: z.array(z.string()),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      cover: image().refine((img) => img.width >= 600, {
+        message: "カバー画像は幅600ピクセル以上でなければなりません",
+      }),
+      tags: z.array(z.string()),
+    }),
 });
 
 // const works = defineCollection({
