598da8e47404c491e799710714d415df08aeac2e
Akira HIRATA <hirataakira6@gmail.com>
2023-09-21 20:42:13 +0900

    add Contact Dialog

--- src/content/config.ts
+++ src/content/config.ts
@@ -14,12 +14,18 @@ const blog = defineCollection({
     }),
 });
 
-// const works = defineCollection({
-//   schema: z.object({
-//     title: z.string(),
-//     description: z.string(),
-//   }),
-// });
+const works = defineCollection({
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      cover: image().refine((img) => img.width >= 600, {
+        message: "カバー画像は幅600ピクセル以上でなければなりません",
+      }),
+      tags: z.array(z.string()),
+    }),
+});
 
-export const collections = { blog };
-// export const collections = { blog, works };
+export const collections = { blog, works };
