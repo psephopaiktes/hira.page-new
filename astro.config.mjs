dad9de23c283876b40dddecb1f5ffefe0aae22e9
Akira HIRATA <hirataakira6@gmail.com>
2023-10-20 02:39:08 +0900

    deploy setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://hira.page",
+  // site: "https://hira.page",
+  site: "https://hira-page-new.vercel.app",
   integrations: [sitemap(), mdx()]
 });
