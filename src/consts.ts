4b393ac9135702ffd337fc7fc30e57424703a7d0
Akira HIRATA <hirataakira6@gmail.com>
2023-10-26 17:53:17 +0900

    fix home

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,4 @@
 export const SITE_TITLE = "WEBA";
 export const SITE_DESCRIPTION = "Welcome to my website!";
+export const ME_DESCRIPTION =
+  "横浜で猫と暮らすデザイナー兼エンジニアです。料理・カラオケ・手品など、なにかを作ったり表現することが趣味。ゲーム・映画・マンガなども嗜みます。チームで協力して新しいモノを作ったり、企画したりするのが得意です。";
